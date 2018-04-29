4b3fcea2743b826ccdebacd2c0c49634df1383d6
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2018-04-29 15:53:56 +0900

    Create string.php

---
+++ string.php
@@ -0,0 +1,24 @@
+<?php
+	function startsWith($haystack, $needle){
+		$length = strlen($needle);
+		return(substr($haystack,0,$length) === $needle);
+	}
+	function endsWith($haystack, $needle){
+		$length = strlen($needle);
+		return($length == 0?true:substr($haystack, -$length) === $needle);
+	}
+	function removeFromEnd($haystack, $needle){
+		return (endsWith($haystack,$needle)?substr($haystack,0,-strlen($needle)):$haystack);
+	}
+	function contains($haystack, $needle){
+		return (strpos((string)$haystack,(string)$needle) >= 0);
+	}
+
+	function replaceSpecialChars($original)
+	{
+		$pattern = array("'é'", "'è'", "'ë'", "'ê'", "'É'", "'È'", "'Ë'", "'Ê'", "'á'", "'à'", "'ä'", "'â'", "'å'", "'Á'", "'À'", "'Ä'", "'Â'", "'Å'", "'ó'", "'ò'", "'ö'", "'ô'", "'Ó'", "'Ò'", "'Ö'", "'Ô'", "'í'", "'ì'", "'ï'", "'î'", "'Í'", "'Ì'", "'Ï'", "'Î'", "'ú'", "'ù'", "'ü'", "'û'", "'Ú'", "'Ù'", "'Ü'", "'Û'", "'ý'", "'ÿ'", "'Ý'", "'ø'", "'Ø'", "'œ'", "'Œ'", "'Æ'", "'ç'", "'Ç'", "'—'", "'−'", "'â€”'", "'’'","'½'");
+		$replace = array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E', 'a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A', 'A', 'o', 'o', 'o', 'o', 'O', 'O', 'O', 'O', 'i', 'i', 'i', 'I', 'I', 'I', 'I', 'I', 'u', 'u', 'u', 'u', 'U', 'U', 'U', 'U', 'y', 'y', 'Y', 'o', 'O', 'ae', 'Ae', 'Ae', 'c', 'C', "-", "-", "-", "'",'{1/2}'); 
+
+		return preg_replace($pattern, $replace, $original);
+	}
+?>
