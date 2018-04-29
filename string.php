e348002781ff1d0c935284bbd12292a72bf60cd4
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2018-04-29 07:02:28 +0000

    Apply fixes from StyleCI

--- string.php
+++ string.php
@@ -2,11 +2,13 @@
     function startsWith($haystack, $needle)
     {
         $length = strlen($needle);
+
         return substr($haystack, 0, $length) === $needle;
     }
     function endsWith($haystack, $needle)
     {
         $length = strlen($needle);
+
         return $length == 0 ? true : substr($haystack, -$length) === $needle;
     }
     function removeFromEnd($haystack, $needle)
@@ -21,8 +23,7 @@
     function replaceSpecialChars($original)
     {
         $pattern = ['/é/', '/è/', '/ë/', '/ê/', '/É/', '/È/', '/Ë/', '/Ê/', '/á/', '/à/', '/ä/', '/â/', '/å/', '/Á/', '/À/', '/Ä/', '/Â/', '/Å/', '/ó/', '/ò/', '/ö/', '/ô/', '/Ó/', '/Ò/', '/Ö/', '/Ô/', '/í/', '/ì/', '/ï/', '/î/', '/Í/', '/Ì/', '/Ï/', '/Î/', '/ú/', '/ù/', '/ü/', '/û/', '/Ú/', '/Ù/', '/Ü/', '/Û/', '/ý/', '/ÿ/', '/Ý/', '/ø/', '/Ø/', '/œ/', '/Œ/', '/Æ/', '/ç/', '/Ç/', '/—/', '/−/', '/â€”/', '/’/', '/½/'];
-    	$replace = ['e', 'e', 'e', 'e', 'E', 'E', 'E', 'E', 'a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A', 'A', 'o', 'o', 'o', 'o', 'O', 'O', 'O', 'O', 'i', 'i', 'i', 'I', 'I', 'I', 'I', 'I', 'u', 'u', 'u', 'u', 'U', 'U', 'U', 'U', 'y', 'y', 'Y', 'o', 'O', 'ae', 'ae', 'Ae', 'c', 'C', '-', '-', '-', "'", '{1/2}'];
+        $replace = ['e', 'e', 'e', 'e', 'E', 'E', 'E', 'E', 'a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A', 'A', 'o', 'o', 'o', 'o', 'O', 'O', 'O', 'O', 'i', 'i', 'i', 'I', 'I', 'I', 'I', 'I', 'u', 'u', 'u', 'u', 'U', 'U', 'U', 'U', 'y', 'y', 'Y', 'o', 'O', 'ae', 'ae', 'Ae', 'c', 'C', '-', '-', '-', "'", '{1/2}'];
 
         return preg_replace($pattern, $replace, $original);
     }
-?>
