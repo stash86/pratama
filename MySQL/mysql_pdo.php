db7b9bee6e784433e83bdf1ccd90a470e7a0d46c
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2018-04-30 05:40:27 +0000

    Apply fixes from StyleCI

--- MySQL/mysql_pdo.php
+++ MySQL/mysql_pdo.php
@@ -4,7 +4,7 @@ function RunInsertQuery($connection, $tablename, $jsonNewRow, $ignoreIfDuplicate
 {
     if (!empty($tablename)) {
         if (!empty($jsonNewRow)) {
-        	require_once __DIR__.'/string.php';
+            require_once __DIR__.'/string.php';
             $jsonNewRow = replaceSpecialChars($jsonNewRow);
             $arrayNewRow = json_decode($jsonNewRow, true);
             if (is_array($arrayNewRow)) {
@@ -41,13 +41,17 @@ function RunInsertQuery($connection, $tablename, $jsonNewRow, $ignoreIfDuplicate
                 if ($stmt->execute()) {
                     return 'Insert success. Id='.$connection->lastInsertId();
                 }
-				return 'insert failed: '.$connection->errorInfo();
+
+                return 'insert failed: '.$connection->errorInfo();
             }
-			return 'Data isn\t in Array format';
+
+            return 'Data isn\t in Array format';
         }
-		return 'No data to be inputted into the table';
-    } 
-	return 'Please provide the name of the database table';
+
+        return 'No data to be inputted into the table';
+    }
+
+    return 'Please provide the name of the database table';
 }
 
 function RunMultipleInsertQuery($connection, $tablename, $arrayColumnNames, $jsonNewRows, $ignoreIfDuplicate = false)
@@ -56,7 +60,7 @@ function RunMultipleInsertQuery($connection, $tablename, $arrayColumnNames, $jso
     if (!empty($tablename)) {
         if (!empty($jsonNewRows)) {
             if (!empty($arrayColumnNames) && is_array($arrayColumnNames)) {
-            	require_once __DIR__.'/string.php';
+                require_once __DIR__.'/string.php';
                 $jsonNewRows = replaceSpecialChars($jsonNewRows);
                 $arrayNewRows = json_decode($jsonNewRows, true);
                 if (is_array($arrayNewRows)) {
@@ -84,131 +88,125 @@ function RunMultipleInsertQuery($connection, $tablename, $arrayColumnNames, $jso
                         }
                     }
                 }
+
                 return 'Data isn\t in Array format';
             }
+
             return 'Please provide the name of the columns in Array format';
         }
+
         return 'No data to be inputted into the table';
     }
+
     return 'Please provide the name of the database table';
 
     if (empty($errorLog)) {
         return 'Insert success.';
     }
+
     return $errorLog;
 }
 
 function RunRawSelectQuery($connection, $sqlQuery)
 {
-	$array = []; 
-	if (!empty($sqlQuery))
-	{
-		$stmt = $connection->prepare($sqlQuery);
-		$stmt->execute();
-		$array = $stmt->fetchAll(PDO::FETCH_ASSOC);
-	}
+    $array = [];
+    if (!empty($sqlQuery)) {
+        $stmt = $connection->prepare($sqlQuery);
+        $stmt->execute();
+        $array = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 
-	return json_encode($array);
+    return json_encode($array);
 }
 
-function RunUpdateQuery($connection, $tablename, $jsonNewRow, $whereQuery='', $jsonWhereParams='')
+function RunUpdateQuery($connection, $tablename, $jsonNewRow, $whereQuery = '', $jsonWhereParams = '')
 {
-	if(!empty($tablename))
-	{
-		if(!empty($jsonNewRow))
-		{
-			require_once __DIR__.'/string.php';
-			$jsonNewRow = replaceSpecialChars($jsonNewRow);
-			$arrayNewRow =json_decode($jsonNewRow,true);
-			if (is_array($arrayNewRow))
-			{
-				$sqlQuery = "UPDATE $tablename SET ";
-		        
-		        foreach ($arrayNewRow as $key => &$val)
-		        {
-					if(empty($val))
-						$sqlQuery .= "$key = DEFAULT,";
-					else
-		    			$sqlQuery .= "$key=:".$key.'_update,';
-				}
+    if (!empty($tablename)) {
+        if (!empty($jsonNewRow)) {
+            require_once __DIR__.'/string.php';
+            $jsonNewRow = replaceSpecialChars($jsonNewRow);
+            $arrayNewRow = json_decode($jsonNewRow, true);
+            if (is_array($arrayNewRow)) {
+                $sqlQuery = "UPDATE $tablename SET ";
 
-		        $sqlQuery = rtrim($sqlQuery,',');
-				if(!empty($whereQuery))
-				{
-					$whereQuery = replaceSpecialChars($whereQuery);
-					$sqlQuery .= " WHERE $whereQuery";
-				}
+                foreach ($arrayNewRow as $key => &$val) {
+                    if (empty($val)) {
+                        $sqlQuery .= "$key = DEFAULT,";
+                    } else {
+                        $sqlQuery .= "$key=:".$key.'_update,';
+                    }
+                }
 
-		        $stmt = $connection->prepare($sqlQuery);
-				foreach ($arrayNewRow as $key => &$val)
-				{
-					if(!empty($val))
-		    			$stmt->bindParam(":".$key.'_update', $val);
-				}
-				if(!empty($jsonWhereParams))
-				{
-					$jsonWhereParams = replaceSpecialChars($jsonWhereParams);
-					foreach (json_decode($jsonWhereParams,true) as $key => &$val)
-					{
-			    		$stmt->bindParam(":".$key, $val);
-					}
-				}
-				
-				if($stmt->execute())
-				{
-					$rowsUpdated = $stmt->rowCount();
-					if($rowsUpdated==1)
-					{
-						return "$rowsUpdated record was successfully updated";
-					}
-					else if($rowsUpdated>1)
-					{
-						return "$rowsUpdated records were successfully updated";
-					}
-					return 'no record updated';
-				}
-				return 'update failed: '.$connection->errorInfo();
-	        }
-	        return 'Data isn\t in Array format';
-		}
-		return 'No data to be inputted into the table';
-	}
-	return 'Please provide the name of the database table';
+                $sqlQuery = rtrim($sqlQuery, ',');
+                if (!empty($whereQuery)) {
+                    $whereQuery = replaceSpecialChars($whereQuery);
+                    $sqlQuery .= " WHERE $whereQuery";
+                }
+
+                $stmt = $connection->prepare($sqlQuery);
+                foreach ($arrayNewRow as $key => &$val) {
+                    if (!empty($val)) {
+                        $stmt->bindParam(':'.$key.'_update', $val);
+                    }
+                }
+                if (!empty($jsonWhereParams)) {
+                    $jsonWhereParams = replaceSpecialChars($jsonWhereParams);
+                    foreach (json_decode($jsonWhereParams, true) as $key => &$val) {
+                        $stmt->bindParam(':'.$key, $val);
+                    }
+                }
+
+                if ($stmt->execute()) {
+                    $rowsUpdated = $stmt->rowCount();
+                    if ($rowsUpdated == 1) {
+                        return "$rowsUpdated record was successfully updated";
+                    } elseif ($rowsUpdated > 1) {
+                        return "$rowsUpdated records were successfully updated";
+                    }
+
+                    return 'no record updated';
+                }
+
+                return 'update failed: '.$connection->errorInfo();
+            }
+
+            return 'Data isn\t in Array format';
+        }
+
+        return 'No data to be inputted into the table';
+    }
+
+    return 'Please provide the name of the database table';
 }
 
-function RunDeleteQuery($connection, $tablename, $whereQuery='', $bindParams='', $ignoreIfDuplicate=false)
+function RunDeleteQuery($connection, $tablename, $whereQuery = '', $bindParams = '', $ignoreIfDuplicate = false)
 {
-	if(!empty($tablename))
-	{
-		if(!empty($whereQuery))
-		{
-			if($ignoreIfDuplicate)
-			{
-				$sql = "DELETE IGNORE FROM $tablename WHERE $whereQuery";
-			}
-			else
-			{
-				$sql = "DELETE FROM $tablename WHERE $whereQuery";
-			}
-			
-			$stmt = $connection->prepare($sql);
-			if (!empty($bindParams))
-			{
-				foreach (json_decode($bindParams,true) as $key => &$val)
-				{
-		    		$stmt->bindParam(":".$key, $val);
-				}
-			}
-			$stmt->execute();
-			$rowsDeleted = $stmt->rowCount();
+    if (!empty($tablename)) {
+        if (!empty($whereQuery)) {
+            if ($ignoreIfDuplicate) {
+                $sql = "DELETE IGNORE FROM $tablename WHERE $whereQuery";
+            } else {
+                $sql = "DELETE FROM $tablename WHERE $whereQuery";
+            }
 
-			if($rowsDeleted>0) 
-			{
-				return "$rowsDeleted rows successfully deleted.";
-			}
-			return 'delete failed: ' . $connection->errorInfo();
-		}
-		return 'Please provide the WHERE clause for this query';
-	}
-	return 'Please provide the name of the database table';
+            $stmt = $connection->prepare($sql);
+            if (!empty($bindParams)) {
+                foreach (json_decode($bindParams, true) as $key => &$val) {
+                    $stmt->bindParam(':'.$key, $val);
+                }
+            }
+            $stmt->execute();
+            $rowsDeleted = $stmt->rowCount();
+
+            if ($rowsDeleted > 0) {
+                return "$rowsDeleted rows successfully deleted.";
+            }
+
+            return 'delete failed: '.$connection->errorInfo();
+        }
+
+        return 'Please provide the WHERE clause for this query';
+    }
+
+    return 'Please provide the name of the database table';
 }
