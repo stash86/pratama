d1b9182555e91c8a493bbf65ff10cf76e5d1ba43
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2018-04-30 01:58:06 +0000

    Apply fixes from StyleCI

--- mana.php
+++ mana.php
@@ -1,56 +1,33 @@
 <?php
-function replaceMagicSymbols($original, $withShadow=false)
+
+function replaceMagicSymbols($original, $withShadow = false)
 {
-	$stringManaCost = "ms-cost";
-	if($withShadow)
-	{
-		$stringManaCost.=" ms-shadow";
-	}
+    $stringManaCost = 'ms-cost';
+    if ($withShadow) {
+        $stringManaCost .= ' ms-shadow';
+    }
 
-	$pattern = [
-		'/\{2\/([\w])\}/' //Example {2/B}
-		,'/\{([BCEGRSUWXYZ\d]+)\}/' //Example {B} or {12}
-		,'/{Q}/'
-		,'/{T}/'
-		,'/{plus}/'
-		,'/\+([\d]+)([:])/' //Example +2:
-		,'/\-([\d]+)([:])/' //Example -2:
-		,'/\{([\w])\/([\w])\}/' //Example {B/G}
-		,'/\{h([\w])\}/' //Example {hb}
-		,'/CHAOS/'
-		,'/Â½/'
-		,'/½/'
-		,'/{1/2}/'
-		,'/{âˆž}/'
-		,'/{∞}/'
-		,'/-X:/'
-	]; 
+    $pattern = [
+        '/\{2\/([\w])\}/' //Example {2/B}
+        , '/\{([BCEGRSUWXYZ\d]+)\}/' //Example {B} or {12}
+        , '/{Q}/', '/{T}/', '/{plus}/', '/\+([\d]+)([:])/' //Example +2:
+        , '/\-([\d]+)([:])/' //Example -2:
+        , '/\{([\w])\/([\w])\}/' //Example {B/G}
+        , '/\{h([\w])\}/' //Example {hb}
+        , '/CHAOS/', '/Â½/', '/½/', '/{1/2}/', '/{âˆž}/', '/{∞}/', '/-X:/',
+    ];
 
-	$replace=[
-		"<i class=\"ms ms-2$1 ms-split $stringManaCost\"></i>"
-		,"<i class=\"ms ms-$1 $stringManaCost\"></i>"
-		,"<i class=\"ms ms-untap $stringManaCost\"></i>"
-		,"<i class=\"ms ms-tap $stringManaCost\"></i>"
-		,'+'
-		,"<i class=\"ms ms-loyalty-up ms-loyalty-$1\"></i>:"
-		,"<i class=\"ms ms-loyalty-down ms-loyalty-$1\"></i>:"
-		,"<i class=\"ms ms-$1$2 ms-split $stringManaCost\"></i>"
-		,"<span class=\"ms-half\"><i class=\"ms ms-$1 $stringManaCost\"></i></span>"
-		,'<i class="ms ms-chaos"></i>'
-		,'<i class="ms ms-1-2"></i>'
-		,'<i class="ms ms-1-2"></i>'
-		,'<i class="ms ms-1-2"></i>'
-		,"<i class=\"ms ms-infinity $stringManaCost\"></i>"
-		,"<i class=\"ms ms-infinity $stringManaCost\"></i>"
-		,"<i class=\"ms ms-loyalty-down ms-loyalty-X\"></i>:"
-	];
+    $replace = [
+        "<i class=\"ms ms-2$1 ms-split $stringManaCost\"></i>", "<i class=\"ms ms-$1 $stringManaCost\"></i>", "<i class=\"ms ms-untap $stringManaCost\"></i>", "<i class=\"ms ms-tap $stringManaCost\"></i>", '+', '<i class="ms ms-loyalty-up ms-loyalty-$1"></i>:', '<i class="ms ms-loyalty-down ms-loyalty-$1"></i>:', "<i class=\"ms ms-$1$2 ms-split $stringManaCost\"></i>", "<span class=\"ms-half\"><i class=\"ms ms-$1 $stringManaCost\"></i></span>", '<i class="ms ms-chaos"></i>', '<i class="ms ms-1-2"></i>', '<i class="ms ms-1-2"></i>', '<i class="ms ms-1-2"></i>', "<i class=\"ms ms-infinity $stringManaCost\"></i>", "<i class=\"ms ms-infinity $stringManaCost\"></i>", '<i class="ms ms-loyalty-down ms-loyalty-X"></i>:',
+    ];
 
-	$new = preg_replace($pattern, $replace, $original);
-	$new = str_replace("0:", "<i class=\"ms ms-loyalty-zero ms-loyalty-0\"></i>:", $new);
-	return $new;
+    $new = preg_replace($pattern, $replace, $original);
+    $new = str_replace('0:', '<i class="ms ms-loyalty-zero ms-loyalty-0"></i>:', $new);
+
+    return $new;
 }
 
 function replaceStartingLoyaltySymbols($original)
 {
-	return "<i class=\"ms ms-loyalty-start ms-loyalty-$original\"></i>";
+    return "<i class=\"ms ms-loyalty-start ms-loyalty-$original\"></i>";
 }
