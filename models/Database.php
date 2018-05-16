7c2bfe0f5e26aa5c99a4d0b30d4857b51b7d676d
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2018-05-16 10:47:09 +0900

    Update Database.php

--- models/Database.php
+++ models/Database.php
@@ -16,7 +16,7 @@ class Database
 
             return $pdo_instance_read;
         } catch (PDOException $e) {
-            $errorMsg = $e->getMessage();
+        	$errorMsg = $e->getMessage();
             echo htmlentities("Connection failed: $errorMsg");
         }
     }
@@ -35,51 +35,47 @@ class Database
 
             return $pdo_instance_write;
         } catch (PDOException $e) {
-            $errorMsg = $e->getMessage();
+        	$errorMsg = $e->getMessage();
             echo htmlentities("Connection failed: $errorMsg");
         }
     }
 
     public static function select($database, $columns, $tables, $where = '', $jsonParams = '', $groupBy = '', $having = '', $order = '', $limit = 0, $offset = 0, $columnData = false)
     {
-        if (!empty($database)) {
-            if (!empty($tables)) {
-                if (!empty($columns)) {
-                    $sql = "SELECT $columns FROM $tables";
-                } else {
-                    $sql = "SELECT * FROM $tables";
-                }
-
-                if (!empty($where)) {
-                    $sql .= " WHERE $where";
-                }
-
-                if (!empty($groupBy)) {
-                    $sql .= " GROUP BY $groupBy";
-                }
-
-                if (!empty($having)) {
-                    $sql .= " HAVING $having";
-                }
-
-                if (!empty($order)) {
-                    $sql .= " ORDER BY $order";
-                }
-
-                if (!empty($limit) && intval($limit) > 0) {
-                    $sql .= " LIMIT $limit";
-                    if (!empty($offset) && intval($offset) > 0) {
-                        $sql .= " OFFSET $offset";
-                    }
-                }
-
-                return self::rawSelectQuery($database, $sql, $jsonParams, $columnData);
+        if (!empty($database) && !empty($tables)) {
+            if (!empty($columns)) {
+                $sql = "SELECT $columns FROM $tables";
+            } else {
+                $sql = "SELECT * FROM $tables";
             }
 
-            return 'Please specify tables\' name for the SELECT query';
+            if (!empty($where)) {
+                $sql .= " WHERE $where";
+            }
+
+            if (!empty($groupBy)) {
+                $sql .= " GROUP BY $groupBy";
+            }
+
+            if (!empty($having)) {
+                $sql .= " HAVING $having";
+            }
+
+            if (!empty($order)) {
+                $sql .= " ORDER BY $order";
+            }
+
+            if (intval($limit) > 0) {
+                $sql .= " LIMIT $limit";
+                if (intval($offset) > 0) {
+                    $sql .= " OFFSET $offset";
+                }
+            }
+
+            return self::rawSelectQuery($database, $sql, $jsonParams, $columnData);
         }
 
-        return 'Please specify which database to be used';
+        return 'Please specify which database and tables to be used';
     }
 
     public static function selectSingleResult($database, $column, $tables, $where = '', $jsonParams = '', $groupBy = '', $having = '', $order = '')
@@ -110,12 +106,10 @@ class Database
 
             $stmt->execute();
             if ($columnData) {
-                $array = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
-            } else {
-                $array = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                return json_encode($stmt->fetchAll(PDO::FETCH_COLUMN, 0));
             }
 
-            return json_encode($array);
+            return json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
         }
 
         return 'Please provide the query to be run';
@@ -124,50 +118,40 @@ class Database
     public static function addRows($database, $tablename, $jsonNewRows, $ignoreIfDuplicate = false)
     {
         $errorLog = '';
-        if (empty($database)) {
-            return 'Please provide the name of the database';
-        }
-        if (empty($tablename)) {
-            return 'Please provide the name of the table';
-        }
-        if (empty($jsonNewRows)) {
-            return 'No data to be inputted into the table';
+        if (empty($database) || empty($tablename) || empty($jsonNewRows)) {
+            return 'One or morw required inputs are empty. Please check!!';
         }
 
         require_once __DIR__.'/../data/string.php';
         if (is_array($jsonNewRows)) {
-            $tempNewRow = json_encode($jsonNewRows);
-        } else {
-            $tempNewRow = $jsonNewRows;
+            $jsonNewRows = json_encode($jsonNewRows);
         }
-        $tempNewRow = replaceSpecialChars($tempNewRow);
-        $arrayNewRows = json_decode($tempNewRow, true);
-        if (is_array($arrayNewRows)) {
-            $arrayUsed = [];
-            if (is_null($arrayNewRows[0])) { // only 1 row of data
-                $arrayUsed[] = $arrayNewRows;
-            } else {
-                $arrayUsed = $arrayNewRows;
+
+        $jsonNewRows = replaceSpecialChars($jsonNewRows);
+        $jsonNewRows = json_decode($jsonNewRows, true);
+        if (is_array($jsonNewRows)) {
+            if (is_null($jsonNewRows[0])) { // not in json format
+            	$jsonNewRows = [$jsonNewRows];
             }
+
             $connection = self::getConnectionWrite($database);
+
+            $sqlQuery = "INSERT INTO $tablename";
             if ($ignoreIfDuplicate) {
                 $sqlQuery = "INSERT IGNORE INTO $tablename";
-            } else {
-                $sqlQuery = "INSERT INTO $tablename";
             }
-            $dataColumns = array_keys($arrayUsed[0]);
+
+            $dataColumns = array_keys($jsonNewRows[0]);
             $sqlQuery .= ' (`'.implode($dataColumns, '`, `').'`) ';
             $sqlQuery .= ' VALUES (:'.implode($dataColumns, ', :').')';
 
             $stmt = $connection->prepare($sqlQuery);
 
-            foreach ($arrayUsed as $data) {
+            foreach ($jsonNewRows as $data) {
                 foreach ($data as $key => &$val) {
                     $stmt->bindParam(':'.$key, $val);
                 }
-                if ($stmt->execute()) {
-                    //return 'Insert success. Id='.$connection->lastInsertId();
-                } else {
+                if (!($stmt->execute())) {
                     $errorLog .= 'insert failed: '.$connection->errorInfo().PHP_EOL;
                 }
             }
@@ -183,79 +167,69 @@ class Database
 
     public static function update($database, $tablename, $jsonNewRow, $whereQuery = '', $jsonWhereParams = '')
     {
-        if (!empty($tablename)) {
-            if (!empty($jsonNewRow)) {
-                require_once __DIR__.'/../data/string.php';
-                if (is_array($jsonNewRow)) {
-                    $tempNewRow = json_encode($jsonNewRow);
-                } else {
-                    $tempNewRow = $jsonNewRow;
-                }
-                $tempNewRow = replaceSpecialChars($tempNewRow);
-                $arrayNewRow = json_decode($tempNewRow, true);
-                if (is_array($arrayNewRow)) {
-                    $sqlQuery = "UPDATE $tablename SET ";
+        if (!empty($tablename) && !empty($jsonNewRow)) {
+            require_once __DIR__.'/../data/string.php';
+            
+            if (is_array($jsonNewRow)) {
+                $jsonNewRow = json_encode($jsonNewRow);
+            }
+            $jsonNewRow = replaceSpecialChars($jsonNewRow);
+            $jsonNewRow = json_decode($jsonNewRow, true);
+            if (is_array($jsonNewRow)) {
+                $sqlQuery = "UPDATE $tablename SET ";
 
-                    foreach ($arrayNewRow as $key => &$val) {
-                        if (empty($val)) {
-                            $sqlQuery .= "$key = DEFAULT,";
-                        } else {
-                            $sqlQuery .= "$key=:".$key.'_update,';
-                        }
+                foreach ($jsonNewRow as $key => &$val) {
+                    if (empty($val)) {
+                        $sqlQuery .= "$key = DEFAULT,";
+                    } else {
+                        $sqlQuery .= "$key=:".$key.'_update,';
                     }
-
-                    $sqlQuery = rtrim($sqlQuery, ',');
-                    if (!empty($whereQuery)) {
-                        $whereQuery = replaceSpecialChars($whereQuery);
-                        $sqlQuery .= " WHERE $whereQuery";
-                    }
-
-                    $connection = self::getConnectionWrite($database);
-                    $stmt = $connection->prepare($sqlQuery);
-                    foreach ($arrayNewRow as $key => &$val) {
-                        if (!empty($val)) {
-                            $stmt->bindParam(':'.$key.'_update', $val);
-                        }
-                    }
-                    if (!empty($jsonWhereParams)) {
-                        if (is_array($jsonWhereParams)) {
-                            $tempWhereParams = json_encode($jsonWhereParams);
-                        } else {
-                            $tempWhereParams = $jsonWhereParams;
-                        }
-                        $tempWhereParams = replaceSpecialChars($tempWhereParams);
-                        foreach (json_decode($tempWhereParams, true) as $key => &$val) {
-                            $stmt->bindParam(':'.$key, $val);
-                        }
-                    }
-
-                    if ($stmt->execute()) {
-                        $rowsUpdated = $stmt->rowCount();
-                        if ($rowsUpdated == 1) {
-                            return "$rowsUpdated record was successfully updated";
-                        } elseif ($rowsUpdated > 1) {
-                            return "$rowsUpdated records were successfully updated";
-                        }
-
-                        return 'no record updated';
-                    }
-
-                    return 'update failed: '.$connection->errorInfo();
                 }
 
-                return 'Data isn\'t in Array format';
+                $sqlQuery = rtrim($sqlQuery, ',');
+                if (!empty($whereQuery)) {
+                    $whereQuery = replaceSpecialChars($whereQuery);
+                    $sqlQuery .= " WHERE $whereQuery";
+                }
+
+                $connection = self::getConnectionWrite($database);
+                $stmt = $connection->prepare($sqlQuery);
+                foreach ($jsonNewRow as $key => &$val) {
+                    if (!empty($val)) {
+                        $stmt->bindParam(':'.$key.'_update', $val);
+                    }
+                }
+                if (!empty($jsonWhereParams)) {
+                    if (is_array($jsonWhereParams)) {
+                        $jsonWhereParams = json_encode($jsonWhereParams);
+                    }
+                    $jsonWhereParams = replaceSpecialChars($jsonWhereParams);
+                    foreach (json_decode($jsonWhereParams, true) as $key => &$val) {
+                        $stmt->bindParam(':'.$key, $val);
+                    }
+                }
+
+                if ($stmt->execute()) {
+                    $rowsUpdated = $stmt->rowCount();
+                    if ($rowsUpdated > 0) {
+                        return "$rowsUpdated record(s) successfully updated";
+                    }
+
+                    return 'no record updated';
+                }
+
+                return 'update failed: '.$connection->errorInfo();
             }
 
-            return 'No data to be inputted into the table';
+            return 'Data isn\'t in Array format';
         }
 
-        return 'Please provide the name of the database table';
+        return 'Please provide all the required data';
     }
 
     public static function delete($database, $tablename, $whereQuery = '', $jsonWhereParams = '', $ignoreIfDuplicate = false)
     {
-        if (!empty($tablename)) {
-            if (!empty($whereQuery)) {
+        if (!empty($tablename) && !empty($whereQuery)) {
                 if ($ignoreIfDuplicate) {
                     $sql = "DELETE IGNORE FROM $tablename WHERE $whereQuery";
                 } else {
@@ -298,12 +272,9 @@ class Database
                 }
 
                 return 'delete failed: '.$connection->errorInfo();
-            }
-
-            return 'Please provide the WHERE clause for this query';
         }
 
-        return 'Please provide the name of the database table';
+        return 'Please provide all the required inputs';
     }
 
     public static function CalculateOffset($currentpage, $currentrow, $limit)
